b5a6959b0eaf33984303f1c00bef4eb444cda84c
Justin Lee <leejustincs@gmail.com>
2025-12-12 06:02:07 -0800

    feat: remove background from terminal screen
    Signed-off-by: Justin Lee <leejustincs@gmail.com>

--- v3/src/app/screens/TerminalScreen.tsx
+++ v3/src/app/screens/TerminalScreen.tsx
@@ -5,12 +5,10 @@ import Background from "../ui/Background";
 
 export default function TerminalScreen({ onNext }: { onNext: () => void }) {
     return (
-        <Background>
-            <div className="absolute inset-0 flex items-center justify-center">
-                <div className="border-[3px] border-bone aspect-[912/610] max-w-[912px] w-full overflow-hidden">
-                    <Terminal />
-                </div>
+        <div className="absolute inset-0 flex items-center justify-center bg-black">
+            <div className="border-[3px] border-bone aspect-[912/610] max-w-[912px] w-full overflow-hidden">
+                <Terminal />
             </div>
-        </Background>
+        </div>
     );
 }
