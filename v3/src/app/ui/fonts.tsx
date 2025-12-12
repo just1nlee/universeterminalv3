91e854c2055c8abe214278546c5fb22fb64c7a1f
Justin Lee <leejustincs@gmail.com>
2025-12-12 03:28:46 -0800

    feat: add font
    Signed-off-by: Justin Lee <leejustincs@gmail.com>

---
+++ v3/src/app/ui/fonts.tsx
@@ -0,0 +1,5 @@
+import { VT323 } from 'next/font/google';
+import { Press_Start_2P } from 'next/font/google';
+
+export const vt323 = VT323({ subsets: ['latin'], weight: ['400'] })
+export const pressStart2P = Press_Start_2P({ subsets: ['latin'], weight: ['400'] })
